045623f26fd74111927cfc1f6e44ef13ee64ca9d
omprasad1997 <20rathodomprasad@gmail.com>
2022-05-15 19:59:01 +0530

    solved pattern22 problem

--- patterns/Patterns.java
+++ patterns/Patterns.java
@@ -4,7 +4,7 @@ package patterns;
 //https://github.com/kunal-kushwaha/DSA-Bootcamp-Java/blob/main/assignments/09-patterns.md
 public class Patterns {
     public static void main(String[] args) {
-        pattern20(5);
+        pattern22(5);
     }
 
     static void pattern1(int n) {
@@ -71,7 +71,7 @@ public class Patterns {
     }
 
     static void pattern6(int n) {
-        for (int row = 1; row <= 2*n - 1; row++) {
+        for (int row = 1; row <= 2 * n - 1; row++) {
             //for every row,run col
             int totalColumnInRow = 1; //any dummy value
             int noOfSpace = 1;
@@ -130,23 +130,126 @@ public class Patterns {
 //            *  *
 //            *  *
 //            ****
-    static void pattern20(int n){
+    static void pattern20(int n) {
         for (int row = 1; row <= n; row++) {
             //for every row,run col
-            if(row == 1 || row == n){
-                for (int col = 1; col <= n-1; col++) {
+            if (row == 1 || row == n) {
+                for (int col = 1; col <= n - 1; col++) {
                     System.out.print("* ");
                 }
             } else {
-                for (int col = 1; col <= n-1; col++) {
-                    if(col == 1 || col == n-1){
+                for (int col = 1; col <= n - 1; col++) {
+                    if (col == 1 || col == n - 1) {
                         System.out.print("* ");
-                    }else {
+                    } else {
                         System.out.print("  ");
                     }
                 }
             }
+            //when one row is printed, add new a line
+            System.out.println();
+        }
+    }
 
+//    1
+//    0 1
+//    1 0 1
+//    0 1 0 1
+//    1 0 1 0 1
+    static void pattern22(int n){
+        int start = 1;
+
+        for(int row = 1; row <= n; row++){
+            if(row % 2 != 0){
+                start = 1;
+            } else {
+                start = 0;
+            }
+
+            boolean hasCheckedOddIndex = false;
+
+            for(int col = 1; col <= row; col++){
+                if(start == 1){
+                    if(!hasCheckedOddIndex) {
+                        System.out.print("1 ");
+                        hasCheckedOddIndex = true;
+                    } else {
+                        System.out.print("0 ");
+                        hasCheckedOddIndex = false;
+                    }
+                } else {
+                    if(!hasCheckedOddIndex) {
+                        System.out.print("0 ");
+                        hasCheckedOddIndex = true;
+                    } else {
+                        System.out.print("1 ");
+                        hasCheckedOddIndex = false;
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern24(int n) {
+        for (int row = 1; row <= n; row++) { //for upper left part
+            //for every row,run col
+            int totalColumnInRow = 1; //any dummy value
+            int noOfSpaces = 1;       //any dummy value
+            if (row <= n) {
+                totalColumnInRow = row; //for upper part when row <= n
+            } else {
+                totalColumnInRow = 2 * n - row; //for lower part when row > n
+            }
+
+            noOfSpaces = n - totalColumnInRow;
+
+
+            if (row == 1 || row == 2) {
+                for (int col = 1; col <= totalColumnInRow; col++) {
+                    System.out.print("* ");
+                }
+
+                for (int space = 1; space <= noOfSpaces; space++) {
+                    System.out.print("  ");
+                }
+
+                for (int space = 1; space <= noOfSpaces; space++) {
+                    System.out.print("  ");
+                }
+
+                for (int col = 1; col <= totalColumnInRow; col++) {
+                    System.out.print("* ");
+                }
+
+            } else {
+                for (int col = 1; col <= totalColumnInRow; col++) {
+                    if (col == 1 || col == totalColumnInRow) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            //when one row is printed, add new a line
+            System.out.println();
+        }
+
+        for (int row = n; row >= 1; row--) { //for down left part
+            //for every row,run col
+            if (row == 1 || row == 2) {
+                for (int col = 1; col <= row; col++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for (int col = 1; col <= row; col++) {
+                    if (col == 1 || col == row) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            }
             //when one row is printed, add new a line
             System.out.println();
         }
@@ -187,7 +290,7 @@ public class Patterns {
                 totalColumnInRow = 2 * n - row; //for lower part when row > n
             }
 
-            noOfSpaces = n  - totalColumnInRow;
+            noOfSpaces = n - totalColumnInRow;
 
             for (int col = 1; col <= totalColumnInRow; col++) {
                 System.out.print("* ");
@@ -243,8 +346,8 @@ public class Patterns {
 
     static void pattern32(int n) {
         for (int row = 1; row <= n; row++) {
-            for (int col = (n+1) - row; col <= n; col++) {
-                char ch = (char)('A' + col - 1);
+            for (int col = (n + 1) - row; col <= n; col++) {
+                char ch = (char) ('A' + col - 1);
                 System.out.print(ch + " ");
             }
             System.out.println();
@@ -258,13 +361,13 @@ public class Patterns {
         for (int row = 1; row <= n; row++) {
             for (int col = row; col >= 1; col--) {
 
-                if(count % 2 == 0){
+                if (count % 2 == 0) {
                     ch = (char) (ch - 32); //ch  - 32 => convert to upper case
                 }
 
                 System.out.print(ch + " ");
-                ch = count % 2 == 0 ? (char)(ch + 32) : ch; //ternary operator => ? :
-                                                            //ch  + 32 => convert to lower case
+                ch = count % 2 == 0 ? (char) (ch + 32) : ch; //ternary operator => ? :
+                //ch  + 32 => convert to lower case
                 ch++;
                 count++;
             }
@@ -275,8 +378,8 @@ public class Patterns {
     static void pattern34(int n) {
         for (int row = 1; row <= n; row++) {
 
-            for (int col = (n+1) - row; col >= 1; col--) {
-                char ch = (char)('A' + col - 1);
+            for (int col = (n + 1) - row; col >= 1; col--) {
+                char ch = (char) ('A' + col - 1);
                 System.out.print(ch + " ");
             }
             System.out.println();
@@ -292,7 +395,7 @@ public class Patterns {
                 System.out.print(col + " ");
             }
 
-            for (int space = 1; space <= 2*totalSpace; space++) { //for first half & second half spaces
+            for (int space = 1; space <= 2 * totalSpace; space++) { //for first half & second half spaces
                 System.out.print("  ");
             }
 
